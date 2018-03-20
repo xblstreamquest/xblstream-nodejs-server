c83fc681cecaac9003419f09a204c759c0a16960
xbl_stream_ip <xbl_stream_ip@outlook.com>
2018-03-19 22:55:12 -0700

    Port Fixed

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ var users = require('./routes/users');
 var app = express();
 
 
-app.set('port', 8000);
 // handlebars view templates hook
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
